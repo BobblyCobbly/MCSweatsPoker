c8f51cdb838b89aebb3a06ef5bb7a837a912e7c0
Cobble8 <drewgenualdo@gmail.com>
2023-10-21 16:50:39 -0400

    fix default shader

--- shaders/default.glsl
+++ shaders/default.glsl
@@ -1,4 +1,4 @@
-#type vertex
+#type vertex;
 #version 330 core
 layout (location = 0) in vec3 aPos;
 layout (location = 1) in vec4 aColor;
@@ -20,7 +20,7 @@ void main() {
     gl_Position = uProjection * uView * vec4(aPos, 1.0);
 }
 
-#type fragment
+#type fragment;
 #version 330 core
 
 in vec4 fColor;
